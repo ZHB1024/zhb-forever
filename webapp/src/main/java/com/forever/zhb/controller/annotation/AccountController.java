4a1e71d776b0bf004aaf0cf5089029e1a9300f73
ZHB1024 <zhb901024>
2018-08-18 23:36:37 +0800

    eclipse commit and push

--- webapp/src/main/java/com/forever/zhb/controller/annotation/AccountController.java
+++ webapp/src/main/java/com/forever/zhb/controller/annotation/AccountController.java
@@ -21,6 +21,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import net.sf.json.JSONObject;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,6 +30,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/htgl/account")
 public class AccountController extends BasicController {
+	
+	private Logger logger = LoggerFactory.getLogger(AccountController.class);
     
     @Resource(name="foreverManager")
     private IForeverManager foreverManager;
